84faaa72fba586eac1333490b503d3156413a77a
phammanh-cloud <ducmanhdv@gmail.com>
2021-09-11 15:50:42 +0700

    update

--- src/sections/Popup/index.js
+++ src/sections/Popup/index.js
@@ -17,9 +17,9 @@ const Popup = () =>{
             const now = new Date().getTime();
             const distance = countDownDate - now;
             const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            // const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            // const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            // const seconds = Math.floor((distance % (1000 * 60)) / 1000);
             document.querySelector(".countdown").innerHTML = days + " day"
               
             if (distance < 0) {
