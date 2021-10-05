d769c91b0f1631bd571e2b3e91316c185ec272f6
phammanh-cloud <ducmanhdv@gmail.com>
2021-10-05 13:57:13 +0700

    update

--- src/sections/Contract/contract.style.js
+++ src/sections/Contract/contract.style.js
@@ -7,15 +7,19 @@ const ContractWrapper = styled.div`
   z-index: 99;
   .contract {
     width: 100%;
-    background: rgba(255, 255, 0, 0.4);
+    background: #ffffff;
     text-align: center;
     border-radius: 5px;
     padding: 10px 0px;
     position: relative;
+    box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px,
+      rgba(6, 24, 44, 0.65) 0px 4px 6px -1px,
+      rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
     p {
       padding: 0px;
       margin: 0;
       font-size: 15px;
+      color: black;
     }
     a {
       color: ${(props) => props.theme.primaryColor};
@@ -24,6 +28,7 @@ const ContractWrapper = styled.div`
     button {
       position: absolute;
       width: unset;
+      background: none;
       height: unset;
       right: 0px;
       top: 0px;
