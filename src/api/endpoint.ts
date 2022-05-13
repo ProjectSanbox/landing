db8281c842358dd51504867183524022dd218cc7
Pham Thanh Ha <hapt@Phams-MacBook-Pro.local>
2022-05-13 08:42:05 +0700

    add new  module

--- src/api/endpoint.ts
+++ src/api/endpoint.ts
@@ -4,6 +4,5 @@ export const Endpoint = {
 
 export const API = {
   base: process.env.REACT_APP_BASE_URL,
-  psb: process.env.REACT_APP_PSB_URL,
   tokenPrice: 'https://api.coingecko.com/api/v3/simple/token_price/',
 }
