08ff1898a029d136c222f3a85f32ccfad830be9e
Pham Thanh Ha <hapt@Phams-MacBook-Pro.local>
2022-05-26 08:42:16 +0700

    fix banner height

--- src/sections/Banner/index.tsx
+++ src/sections/Banner/index.tsx
@@ -5,7 +5,7 @@ import { BannerContent, BannerWrapper, BigTitle, DownloadButton, DownloadButtonW
 
 const Banner = () => {
   return (
-    <BannerWrapper height="100vh" background={headerBackground} id={SupportedBlockId.HOME}>
+    <BannerWrapper height="calc(100vh + 60px)" background={headerBackground} id={SupportedBlockId.HOME}>
       <div className="overlay"></div>
       <BannerContent>
         <SmallTitle data-aos="fade-up" className="heading">
