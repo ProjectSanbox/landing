fceb67029ce1b9e3820002adbac6664e71a7bd91
Pham Thanh Ha <hapt@Phams-MacBook-Pro.local>
2022-05-27 16:08:27 +0700

    update land overlay background

--- src/sections/BuildLand/index.tsx
+++ src/sections/BuildLand/index.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Heading, LandWrapper, SectionContent } from './styled'
-import { landBackground } from 'assets'
+import landBackground from 'assets/images/land/background.png'
 import { useAppSelector } from 'state/hooks'
 import { selectLands } from 'state/global/global.slice'
 import { RenderItems } from './components'
@@ -9,13 +9,14 @@ type BuildLandProps = {}
 
 const BuildLand: FC<BuildLandProps> = () => {
   const lands = useAppSelector(selectLands)
+  const [sectionBg, setSectionBg] = useState(landBackground)
   return (
-    <LandWrapper height="1170px" background={landBackground}>
+    <LandWrapper height="1178px" background={sectionBg}>
       <SectionContent>
         <Heading>Build lands + share</Heading>
         <Heading>Passive Income</Heading>
         <p style={{ marginTop: '1rem' }}>Coming Soon</p>
-        <RenderItems lands={lands} />
+        <RenderItems lands={lands} setSectionBg={setSectionBg} />
       </SectionContent>
     </LandWrapper>
   )
